2aa4180b9012c39825f78335f6974121da11a3f2
Berta Devant <bertadeva@gmail.com>
2016-06-18 17:52:53 -0400

    adding to do list comments on pages

---
+++ HavenOnDemand.js
@@ -0,0 +1,4 @@
+/*TODO: write connection to the Haven on demand API
+        Sentiments: need functions that accepts a input and returns if positive or negative
+        Related Search: gets a keyword from input checks with categoriesKey.js which category input is from
+                        does a related search and adds those keywords to the category*/
